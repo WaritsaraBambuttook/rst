6302ae2dbc966c19c8d495db412e9b206847140b
WaritsaraBambuttook <four_nukiz@hotmail.com>
2020-10-24 02:54:18 +0700

    route

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,39 @@
   <div class="app">
     <q-layout>
       <div>
-        <q-toolbar class="text-white shadow-2 toolbar">
-          <q-img
-            class="logo "
-            src="https://static.wixstatic.com/media/74c934_d8b1b749bd8e452288b7a311691e3f8f~mv2.png/v1/crop/x_0,y_16,w_429,h_507/fill/w_53,h_60,al_c,q_85,usm_0.66_1.00_0.01/logo%20mobile%20key-02.webp"
-            style="height: 60px; max-width: 53px"
-          />
-          <!-- <q-btn flat label="Homepage"  /> -->
-          <q-space />
-
-          <!--
-        notice shrink property since we are placing it
-        as child of QToolbar
-      -->
-          <q-tabs shrink stretch class="tabbar">
-            <q-tab name="tab1" to="/" label="หน้าแรก" />
-            <q-tab name="tab2" to="/Kyc" label="ระบบ e-KYC" />
-            <q-tab name="tab3" to="/ESignature" label="ระบบ e-Signature" />
-            <q-tab
-              name="tab4"
-              to="/iSignSecure"
-              label="เทคโนโลยีของ ISignSecure"
+        <q-header>
+          <q-toolbar class="text-white shadow-2 toolbar">
+            <q-img
+              class="logo "
+              src="https://static.wixstatic.com/media/74c934_d8b1b749bd8e452288b7a311691e3f8f~mv2.png/v1/crop/x_0,y_16,w_429,h_507/fill/w_53,h_60,al_c,q_85,usm_0.66_1.00_0.01/logo%20mobile%20key-02.webp"
+              style="height: 60px; max-width: 53px"
             />
-            <q-tab name="tab5" to="/Contact" label="ติดต่อเรา" />
-          </q-tabs>
-        </q-toolbar>
+            <q-space />
+            <q-tabs shrink stretch class="tabbar">
+              <q-route-tab name="tab1" :to="{ name: 'Home' }" label="หน้าแรก" />
+              <q-route-tab
+                name="tab2"
+                :to="{ name: 'Kyc' }"
+                label="ระบบ e-KYC"
+              />
+              <q-route-tab
+                name="tab3"
+                :to="{ name: 'ESignature' }"
+                label="ระบบ e-Signature"
+              />
+              <q-route-tab
+                name="tab4"
+                :to="{ name: 'ISignSecure' }"
+                label="เทคโนโลยีของ ISignSecure"
+              />
+              <q-route-tab
+                name="tab5"
+                :to="{ name: 'Contact' }"
+                label="ติดต่อเรา"
+              />
+            </q-tabs>
+          </q-toolbar>
+        </q-header>
       </div>
 
       <div>
